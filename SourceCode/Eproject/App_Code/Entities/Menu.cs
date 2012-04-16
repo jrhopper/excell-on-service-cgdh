fbbc0fd119b2ab197e4ee1b59b0f635a6ab8597d
qchuong100 <qchuong100@1634bd79-5e2b-65f8-74c1-d9c28bf36035>
2012-04-16 10:58:01 +0000

    update entities qchuong100

---
+++ SourceCode/Eproject/App_Code/Entities/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Entities
+{
+    public class Menu
+    {
+        String id;
+        String name;
+		String link;
+        String description;
+       
+		public String Link
+        {
+            get { return link; }
+            set { link = value; }
+        }            
+
+        public String Id
+        {
+            get { return id; }
+            set { id = value; }
+        }        
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }        
+
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+    }
+}
---
+++ SourceCode/Eproject/App_Code/Entities/ServiceOrder.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Entities
+{
+    public class ServiceOrder
+    {
+        String id;
+		String empId;
+        String name;
+        String customer;
+        String serviceType;
+        String beginDate;
+        String endDate;
+        String complete;
+        String description;
+
+		public String EmpId
+        {
+            get { return empId; }
+            set { empId = value; }
+        }
+		
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public String Complete
+        {
+            get { return complete; }
+            set { complete = value; }
+        }
+
+        public String EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        public String BeginDate
+        {
+            get { return beginDate; }
+            set { beginDate = value; }
+        }
+
+        public String ServiceType
+        {
+            get { return serviceType; }
+            set { serviceType = value; }
+        }
+
+        public String Customer
+        {
+            get { return customer; }
+            set { customer = value; }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public String Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
