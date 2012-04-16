88a7886867facbf49d3a842f4e45a57759569528
daile1503 <daile1503@1634bd79-5e2b-65f8-74c1-d9c28bf36035>
2012-04-16 10:21:54 +0000

    dai

---
+++ trunk/Linh tinh/dai/ServiceOrder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Entities
+{
+    public class ServiceOrder
+    {
+        String id;
+        String name;
+        String customer;
+        String servicetype;
+        String begindate;
+        String enddate;
+        String complete;
+        String description;
+
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public String Complete
+        {
+            get { return complete; }
+            set { complete = value; }
+        }
+
+        public String Enddate
+        {
+            get { return enddate; }
+            set { enddate = value; }
+        }
+
+        public String Begindate
+        {
+            get { return begindate; }
+            set { begindate = value; }
+        }
+
+        public String Servicetype
+        {
+            get { return servicetype; }
+            set { servicetype = value; }
+        }
+
+        public String Customer
+        {
+            get { return customer; }
+            set { customer = value; }
+        }
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public String Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
---
+++ trunk/Linh tinh/dai/ServiceType.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Entities
+{
+    public class ServiceType
+    {
+        String id;
+        String name;
+        String charges;
+        String description;
+
+        public String Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        String image;
+
+        public String Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+        
+
+        public String Charges
+        {
+            get { return charges; }
+            set { charges = value; }
+        }
+        
+
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+    }
+}
