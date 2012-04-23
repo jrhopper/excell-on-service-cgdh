4464bf2d747808cb794e5ad3aa8ddd43666c5973
tronggia <tronggia@gmail.com>
2012-04-23 02:03:27 +0000

    Update code Print

---
+++ Linh tinh/Print/ManageDepartment.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Admin_ManageDepartment : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {        
+        ConfigurationDAL c = new ConfigurationDAL();
+        c.connectDB();
+        c.openConnect();
+        DepartmentBLL depBLL = new DepartmentBLL();
+        gvDepartment.DataSource = depBLL.getAllDepartment();
+        gvDepartment.DataBind();
+        c.closeConnect();
+    }
+    protected void gvDepartment_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnPrint_Click(object sender, EventArgs e)
+    {
+        Session["ctrl"] = gvDepartment;
+        ClientScript.RegisterStartupScript(this.GetType(), "onclick", "<script language=javascript>window.open('Print.aspx','PrintMe','height=768px,width=1366px,scrollbars=1');</script>");
+    }
+}
