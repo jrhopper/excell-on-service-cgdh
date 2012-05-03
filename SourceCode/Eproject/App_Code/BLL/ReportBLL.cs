b35213d4df5d6199ff6d902f5325bd6fd3be8b44
qchuong100 <qchuong100@1634bd79-5e2b-65f8-74c1-d9c28bf36035>
2012-05-03 07:52:19 +0000

    report

---
+++ SourceCode/Eproject/App_Code/BLL/ReportBLL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for ReportBLL
+/// </summary>
+public class ReportBLL
+{
+    ReportDAL reportDAL = new ReportDAL();
+	public ReportBLL()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    /*
+     Author:La Quoc Chuong
+     * Purpose: report
+     */
+    public List<Report> report(String serviceorderid, String month, String empid, String custid)
+    {
+        return reportDAL.report(serviceorderid,month,empid,custid);
+    }
+
+    /*
+    *Author:La Quoc Chuong
+    *Purpose: report count
+    *Date: 2012/04/17
+    */
+    public int reportCount(String serviceorderid, String month, String empid, String custid)
+    {
+        return reportDAL.reportCount(serviceorderid,month,empid,custid);
+    }
+}
